7d873d83b4f0df6519f81296fd83a366777065e0
amantth <amantarekegn01@gmail.com>
2026-05-15 16:34:07 +0300

    Add Jenkins CI/CD and Docker deployment config

--- backend/conference_backend/urls.py
+++ backend/conference_backend/urls.py
@@ -6,9 +6,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('conference.urls')),
+    
 ]
 
 # Serve media files in development
